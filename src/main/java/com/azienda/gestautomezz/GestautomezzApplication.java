2dc2f9cf5c3e974e0da65eae6651209ba3912675
DavideGaliano <jconstantin@tiscali.it>
2025-02-25 20:15:01 +0100

    starter

---
+++ src/main/java/com/azienda/gestautomezz/GestautomezzApplication.java
@@ -0,0 +1,13 @@
+package com.azienda.gestautomezz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestautomezzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestautomezzApplication.class, args);
+	}
+
+}
